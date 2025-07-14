b5e0b08469b07907485357c6fcafb73d739e8f19
harshparadkar25 <harshparadkar25@gmail.com>
2025-07-14 20:38:04 +0530

    changing cookies config

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -85,7 +85,7 @@ const loginUser = async(req,res)=>{
     res
   .cookie("token", token, {
     httpOnly: true,
-    secure: false,           
+    secure: true,           
     sameSite: "Lax",      
   })
   .json({
